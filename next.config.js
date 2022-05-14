6133f8ecf713604f04274ff236bc131a3589a4de
上田貴央 <gibson511511@gmail.com>
2022-05-14 23:26:26 +0900

    yarn dev done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	webpackDevMiddleware: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 200,
+		}
+		return config
+	},
 }
 
 module.exports = nextConfig
