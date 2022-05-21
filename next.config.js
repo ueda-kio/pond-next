82f02bb6f255381efaea261656dd0fd3fe9a2f11
上田貴央 <gibson511511@gmail.com>
2022-05-22 01:45:31 +0900

    cms setting

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
 		}
 		return config
 	},
+	webpack: (cfg) => {
+		cfg.module.rules.push({
+			test: /\.md$/,
+			loader: 'frontmatter-markdown-loader',
+			options: {
+				mode: ['react-component']
+			}
+		})
+		return cfg;
+	}
 }
 
 module.exports = nextConfig
