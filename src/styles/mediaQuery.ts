5b64d9115357173c04c3bc131a4c102bac08be29
上田貴央 <gibson511511@gmail.com>
2022-07-24 00:06:05 +0900

    mediaQuery fixed

--- src/styles/mediaQuery.ts
+++ src/styles/mediaQuery.ts
@@ -1,43 +1,9 @@
-export const breakpoints = {
-	sp: 640,
-	pad: 768,
-	pc: 1080,
-	xl: 1280,
-	'2xl': 1536,
-} as const;
-
-type DeviceType = 'sp' | 'tablet' | 'pc' | 'xl' | '2xl';
-type Media = {
-	[key in DeviceType]: string;
+const mediaQuery = {
+	sp: '@media (min-width: 640px)',
+	tablet: '@media (min-width: 768px)',
+	pc: '@media (min-width: 1080px)',
+	xl: '@media (min-width: 1280px)',
+	xxl: '@media (min-width: 1536px)',
 };
-
-const breakpoint = [
-	{
-		id: 'sp',
-		breakpoint: 640,
-	},
-	{
-		id: 'tablet',
-		breakpoint: 768,
-	},
-	{
-		id: 'pc',
-		breakpoint: 1080,
-	},
-	{
-		id: 'xl',
-		breakpoint: 1280,
-	},
-	{
-		id: 'tablet',
-		breakpoint: 1536,
-	},
-];
-
-export const mediaQuery = breakpoint.reduce((_, { id, breakpoint }) => {
-	return {
-		[id]: `@media (min-width: ${breakpoint}px)`,
-	};
-}, {}) as Media;
 
 export default mediaQuery;
